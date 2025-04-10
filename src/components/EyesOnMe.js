1bff6a25bb760e1521617aa4b8b76827f8f658f7
Mellon Obada <mellobadah123@gmail.com>
2025-04-10 16:50:04 +0300

    events first

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe (){
+
+    const handleFocus = () =>{
+ console.log('Good!');
+    }
+    
+    const handleBlue =() =>{
+        console.log('Hey! Eyes on me!'); 
+    }
+    return (
+        <div>
+            <button onFocus={handleFocus} onBlur={handleBlue}> Eyes on me</button>
+        </div>
+    )
+}
+export default EyesOnMe
